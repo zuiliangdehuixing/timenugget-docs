51ddece9730d76aa2c1929982d4a664db115458a
Redstonexs <154490540+Redstonexs@users.noreply.github.com>
2025-06-04 18:24:54 +0800

    Update index.js

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,29 +1,17 @@
-// https://vitepress.dev/guide/custom-theme
-<% if (!defaultTheme) { %>import Layout from './Layout.vue'<% if (useTs) { %>
-import type { Theme } from 'vitepress'<% } %>
-import './style.css'
-
-<% if (!useTs) { %>/** @type {import('vitepress').Theme} */
-<% } %>export default {
-  Layout,
-  enhanceApp({ app, router, siteData }) {
-    // ...
-  }
-}<% if (useTs) { %> satisfies Theme<% } %>
-<% } else { %>import { h } from 'vue'<% if (useTs) { %>
-import type { Theme } from 'vitepress'<% } %>
+// .vitepress/theme/index.js
+import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
 
-<% if (!useTs) { %>/** @type {import('vitepress').Theme} */
-<% } %>export default {
+export default {
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
+      // 可以在此处添加自定义插槽内容
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
     })
   },
   enhanceApp({ app, router, siteData }) {
-    // ...
+    // 可以在此处注册全局组件或添加自定义功能
   }
-}<% if (useTs) { %> satisfies Theme<% } %><% } %>
+}
