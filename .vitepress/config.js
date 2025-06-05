70d9ae4c109b18b7e20546370289833e3436854c
Dabbler <dabbler6219@outlook.com>
2025-06-05 19:41:22 +0800

    Update config.js

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
 
-    logo: "/logo.png",
+    logo: "/avatar.png",
 
     nav: [
       { text: "首页", link: "/" },
